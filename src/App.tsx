9fc56818fa2cd6e473e015188c4764c08286c62a
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-07-06 23:20:04 -0300

    :up:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Olá Mundo!
         </p>
         <a
           className="App-link"
@@ -22,5 +17,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
